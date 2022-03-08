ef1185b94c05725fe8cc99f16429f34dd9f91e0a
Ben Coepp <bencoepp@gmail.com>
2022-03-08 17:59:06 +0100

    adding

--- lib/Modules/Sessions.dart
+++ lib/Modules/Sessions.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_import, file_names
+
 import 'package:flutter/material.dart';
 
 class Session {
---
+++ lib/data.dart
@@ -0,0 +1,7 @@
+import 'package:data_visualization_tool/Modules/Projects.dart';
+import 'package:data_visualization_tool/Modules/Sessions.dart';
+import 'package:data_visualization_tool/Modules/User.dart';
+
+User currentUser = User(name: '', userName: '', password: '', token: '');
+List<Project> projectList = [];
+List<Session> sessionList = [];
